08ba66150ba4ec7b4b0bb96b076ff5cf764b7490
Sam Blake <samuel.thomas.blake@gmail.com>
2023-03-12 18:26:28 +1100

    Add files via upload

--- aerial_imagery_classifier.py
+++ aerial_imagery_classifier.py
@@ -25,6 +25,8 @@ plt.rcParams["figure.figsize"] = (10, 6)
 # Some quick plotting routines. 
 
 def show_greyscale_image(image, title = None, axis = 'off'):
+    """Quick plotting routine for greyscale images using matplotlib. \
+    Options are title = None, axis = 'off'."""
     fig, ax = plt.subplots() 
     ax.imshow(image, interpolation='nearest', cmap=plt.get_cmap('Greys'))
     ax.axis(axis)
@@ -37,6 +39,8 @@ def show_greyscale_image(image, title = None, axis = 'off'):
 
 
 def show_bgr_image(image, title = None, axis = 'off'):
+    """Quick plotting routine for BGR images using matplotlib. \
+    Options are title = None, axis = 'off'."""
     fig, ax = plt.subplots() 
     ax.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     ax.axis(axis)
@@ -49,6 +53,8 @@ def show_bgr_image(image, title = None, axis = 'off'):
 
 
 def show_hls_image(image, title = None, axis = 'off'):
+    """Quick plotting routine for HLS images using matplotlib. \
+    Options are title = None, axis = 'off'."""
     fig, ax = plt.subplots() 
     ax.imshow(cv2.cvtColor(image, cv2.COLOR_HLS2RGB))
     ax.axis(axis)
@@ -61,6 +67,8 @@ def show_hls_image(image, title = None, axis = 'off'):
 
 
 def show_lab_image(image, title = None, axis = 'off'):
+    """Quick plotting routine for Lab images using matplotlib. \
+    Options are title = None, axis = 'off'."""
     fig, ax = plt.subplots() 
     ax.imshow(cv2.cvtColor(image, cv2.COLOR_Lab2RGB))
     ax.axis(axis)
@@ -73,6 +81,8 @@ def show_lab_image(image, title = None, axis = 'off'):
 
 
 def show_bgra_image(image, title = None, axis = 'off'):
+    """Quick plotting routine for BGRA (blue, green, red, alpha) images \
+    using matplotlib. Options are title = None, axis = 'off'."""
     fig, ax = plt.subplots() 
     ax.imshow(cv2.cvtColor(image, cv2.COLOR_BGRA2RGBA))
     ax.axis(axis)
@@ -85,6 +95,8 @@ def show_bgra_image(image, title = None, axis = 'off'):
 
 
 def heatmap(image, cmap = 'hot_r', colorbar = False, title = None, axis = 'off'):
+    """Quick plotting routine for heat maps using matplotlib. \
+    Options are colorbar = False, title = None, axis = 'off'."""
     fig, ax = plt.subplots()
     im = ax.imshow(image, interpolation='bilinear', cmap=cmap, origin='upper')
     ax.axis(axis)
@@ -99,7 +111,7 @@ def heatmap(image, cmap = 'hot_r', colorbar = False, title = None, axis = 'off')
 
 
 def show_bgr_image_histogram(image, title = None):
-    
+    """Histogram of blue, green and red components of an image. Currently under development."""
     blue = image[:,:,0]
     green = image[:,:,1]
     red = image[:,:,2]
@@ -114,6 +126,7 @@ def show_bgr_image_histogram(image, title = None):
 
 
 def histogram_equalise(img):
+    """Adaptive histogram equalisation for BGR images."""
     img_hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
     luma = img_hls[:,:,1]
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(16, 16))
@@ -125,7 +138,7 @@ def histogram_equalise(img):
 
 
 def plot_spectra_table_bgr(spectra, ncols = 25, npx_per_row = 32, title = None):
-    
+    """Plot table of spectra in BGR colour space."""
     nrows = int(np.ceil(len(spectra)/ncols))
     image = np.zeros((npx_per_row*nrows, npx_per_row*ncols, 3), dtype = np.uint8)
     image[:,:,:] = 255 # white background
@@ -148,7 +161,8 @@ def plot_spectra_table_bgr(spectra, ncols = 25, npx_per_row = 32, title = None):
 
 
 def plot_spectra_table_hls(spectra, ncols = 25, npx_per_row = 32, title = None):
-    
+    """Plot table of spectra in HLS colour space."""
+
     nrows = int(np.ceil(len(spectra)/ncols))
     image = np.zeros((npx_per_row*nrows, npx_per_row*ncols, 3), dtype = np.uint8)
     image[:,:,:] = 255 # white background
@@ -169,6 +183,7 @@ def plot_spectra_table_hls(spectra, ncols = 25, npx_per_row = 32, title = None):
 
 
 def plot_spectra_table_lab(spectra, ncols = 25, npx_per_row = 32, title = None):
+    """Plot table of spectra in Lab colour space."""
     
     nrows = int(np.ceil(len(spectra)/ncols))
     image = np.zeros((npx_per_row*nrows, npx_per_row*ncols, 3), dtype = np.uint8)
@@ -247,6 +262,38 @@ def image_spectra_diff(image, spectra, classification):
 
 
 
+@jit(nopython=True,fastmath=True)
+def image_spectra_diff_weighted(image, spectra, weights, classification):
+    """Computes the RMSE of each pixel in the image with the sample spectra datasets."""
+
+    nrows,ncols = classification.shape
+
+    weight_sum = np.sum(weights)
+
+    for i in range(nrows):
+        for j in range(ncols):
+
+            spectra_min = 1.
+            spectra_weight = 0.
+
+            for s,w in zip(spectra, weights):
+                # root-mean-square error
+                diff = (image[i,j,0] - s[0])**2 + \
+                       (image[i,j,1] - s[1])**2 + \
+                       (image[i,j,2] - s[2])**2
+                diff /= 3.
+                diff = np.sqrt(diff)
+                # min error -- our analogue for the closest spectral match
+                if diff < spectra_min:
+                    spectra_min = diff
+                    spectra_weight = w
+
+            classification[i,j] = (spectra_weight/weight_sum)*spectra_min
+
+    return
+
+
+
 def denoise(image, morph_close = 20, morph_open = 20):
     """denoise requires a 4-channel BGRA image."""
     img_bw = cv2.cvtColor(image, cv2.COLOR_BGRA2GRAY)
@@ -271,7 +318,7 @@ def denoise(image, morph_close = 20, morph_open = 20):
 
 # Reference for Lab colour space: https://docs.opencv.org/4.x/de/d25/imgproc_color_conversions.html#color_convert_rgb_lab
 
-def imagery_statistical_binary_classification(image_bgr, samples_A_bgr, samples_B_bgr, 
+def imagery_statistical_binary_classification(input_image_bgr, samples_A_bgr, samples_B_bgr, 
                      image_blur_kernel_size = (3,3),
                      image_resize_ratio = 10,
                      template_blur_kernel_size = (3,3), 
@@ -280,29 +327,33 @@ def imagery_statistical_binary_classification(image_bgr, samples_A_bgr, samples_
                      duplicate_tolerance = 5, 
                      morph_close = 20, 
                      morph_open = 20,
+                     equalise = True, 
                      denoise_classification_map = True, 
                      dilation_size = 100, 
                      half_normal_dist = False,
                      imagery_id = None,
                      export_histogram = False, 
                      export_heatmap = False, 
+                     preprocess_image = True, 
                      plotting = False,
                      verbose = False):
- 
+
     # Image ID string for plot labels. 
     if imagery_id is not None:
-        id_str = os.path.basename(imagery_id).replace('.JPG','')
+        id_str = os.path.basename(imagery_id).replace('.JPG','').replace('.tif','')
     else:
         id_str = 'UNKNOWN'
 
     if plotting:
-        show_bgr_image(image_bgr, title = f'{id_str} - original image')
+        show_bgr_image(input_image_bgr, title = f'{id_str} - original image')
 
     # Histogram equalisation. 
-    image_bgr = histogram_equalise(image_bgr)
-
-    if plotting:
-        show_bgr_image(image_bgr, title = f'{id_str} - histogram equalised')
+    if equalise:
+        image_bgr = histogram_equalise(input_image_bgr)
+        if plotting:
+            show_bgr_image(image_bgr, title = f'{id_str} - histogram equalised')
+    else:
+        image_bgr = input_image_bgr.copy()
 
     # Convert from BGR to HSL. 
     image = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2Lab)
@@ -345,7 +396,7 @@ def imagery_statistical_binary_classification(image_bgr, samples_A_bgr, samples_
 
     # Remove similar spectra common to both weeds and crop. 
     samples_A_preprocessed, samples_B_preprocessed = remove_common_spectra(\
-         samples_A_preprocessed, samples_B_preprocessed, tol = duplicate_tolerance)
+          samples_A_preprocessed, samples_B_preprocessed, tol = duplicate_tolerance)
     
     # Remove near duplicates. 
     samples_A_preprocessed, samples_A_weights = remove_near_duplicate_spectra(\
@@ -373,10 +424,13 @@ try decreasing \'duplicate_tolerance\', which is currently {duplicate_tolerance}
         print(f'number of distinct spectra in sample B is {len(samples_B_preprocessed)}')
     
     # Preprocess image. 
-    image_pre = cv2.blur(image, image_blur_kernel_size)
-    image_pre = cv2.resize(image_pre, 
-        (image.shape[1]//image_resize_ratio, image.shape[0]//image_resize_ratio), 
-        interpolation = cv2.INTER_AREA)
+    if preprocess_image:
+        image_pre = cv2.blur(image, image_blur_kernel_size)
+        image_pre = cv2.resize(image_pre, 
+            (image.shape[1]//image_resize_ratio, image.shape[0]//image_resize_ratio), 
+            interpolation = cv2.INTER_AREA)
+    else:
+        image_pre = image.copy()
 
     if plotting:
         show_lab_image(image_pre, title = f'{id_str} - preprocessed image')
@@ -391,6 +445,9 @@ try decreasing \'duplicate_tolerance\', which is currently {duplicate_tolerance}
     image_spectra_diff(image_pre, samples_A_preprocessed, density_A)
     image_spectra_diff(image_pre, samples_B_preprocessed, density_B)
 
+    # image_spectra_diff_weighted(image_pre, samples_A_preprocessed, samples_A_weights, density_A)
+    # image_spectra_diff_weighted(image_pre, samples_B_preprocessed, samples_B_weights, density_B)
+
     # Experimental - limit range of outliers. 
     ### threshold = np.percentile(density_A, 10.)
     ### density_A[density_A > threshold] = threshold
@@ -404,6 +461,10 @@ try decreasing \'duplicate_tolerance\', which is currently {duplicate_tolerance}
     density_B = cv2.resize(density_B, (image.shape[1], image.shape[0]), interpolation = cv2.INTER_CUBIC)
     density   = cv2.resize(density,   (image.shape[1], image.shape[0]), interpolation = cv2.INTER_CUBIC)
     
+    # Mask no_data (0,0,0) for geotiff.
+    image_sum = np.sum(input_image_bgr, axis = 2)
+    density[image_sum == 0] = np.nan
+
     # Plot histograms of spectral match difference. 
     if plotting:
         fig, ax = plt.subplots()
@@ -508,6 +569,7 @@ try decreasing \'duplicate_tolerance\', which is currently {duplicate_tolerance}
     classification_image = np.full(image.shape, 255, dtype = np.uint8) 
     classification_image = cv2.cvtColor(classification_image, cv2.COLOR_RGB2RGBA) # alpha channel
     classification_image[:,:,3] = 0
+
     for i,n_sigma in enumerate(n_sigma_thresholds,1):
         classification_image[density < mean - n_sigma*std] = colours255[i]
 
@@ -517,31 +579,36 @@ try decreasing \'duplicate_tolerance\', which is currently {duplicate_tolerance}
     # Denoise classification map. 
     if denoise_classification_map:
         classification_image = denoise(classification_image, morph_close = morph_close, morph_open = morph_open)
-
         if plotting:
             show_bgra_image(classification_image, title = f'{id_str} - Denoised Classification Map')
 
-    # Dilate to generate spray region (an overestimate of the area enclosing the weeds.) The array 
-    # spray_region is a binary image, where 0 -> no spray, 1 -> spray. 
-    spray_region = np.zeros((image.shape[0], image.shape[1]), dtype = np.uint8)
-    # for i,n_sigma in enumerate(n_sigma_thresholds,1):
-    #     spray_region[density < mean - n_sigma*std] = 1
-    spray_region[density < mean - max(n_sigma_thresholds)*std] = 1
-
-    denoise_kernel = np.ones((dilation_size//10, dilation_size//10), dtype = np.uint8)
-    spray_region = cv2.dilate(spray_region, denoise_kernel, iterations = 1)
+    # Create binary classification. 
+    classification_binary = np.zeros((image.shape[0], image.shape[1]), dtype = np.uint8)
+    classification_binary[classification_image[:,:,3] == 255] = 1
 
     if plotting:
-        fig, ax = plt.subplots()
-        im = ax.imshow(spray_region, interpolation='bilinear', cmap='Pastel1_r',
-                       origin='upper', vmin=0., vmax=1.)
-        ax.axis('off')
-        plt.title(f'{id_str} - Spray Region')
-        plt.colorbar(im,fraction=0.046*spray_region.shape[0]/spray_region.shape[1], pad=0.04)
-        plt.savefig(f'{id_str}_SPRAY_REGION.PNG')
-        plt.show() 
+        show_greyscale_image(255*classification_binary, title = f'{id_str} - Binary Classification Map')
 
-    return density, classification_image, spray_region
+
+    # Dilate to generate spray region (an overestimate of the area enclosing the weeds.) The array 
+    # spray_region is a binary image, where 0 -> no spray, 1 -> spray. 
+    ### spray_region = np.zeros((image.shape[0], image.shape[1]), dtype = np.uint8)
+    ### spray_region[density < mean - max(n_sigma_thresholds)*std] = 1
+
+    ### denoise_kernel = np.ones((dilation_size//10, dilation_size//10), dtype = np.uint8)
+    ### spray_region = cv2.dilate(spray_region, denoise_kernel, iterations = 1)
+
+    ### if plotting:
+    ###     fig, ax = plt.subplots()
+    ###     im = ax.imshow(spray_region, interpolation='bilinear', cmap='Pastel1_r',
+    ###                    origin='upper', vmin=0., vmax=1.)
+    ###     ax.axis('off')
+    ###     plt.title(f'{id_str} - Spray Region')
+    ###     plt.colorbar(im,fraction=0.046*spray_region.shape[0]/spray_region.shape[1], pad=0.04)
+    ###     plt.savefig(f'{id_str}_SPRAY_REGION.PNG')
+    ###     plt.show() 
+
+    return density, classification_image, classification_binary
 
 
 
